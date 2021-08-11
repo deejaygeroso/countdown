3da4d297cd1194668bddc63201cde756b9bee54f
Deejay Geroso <deejaygeroso@gmail.com>
2021-08-11 17:37:03 +0800

    Removed unused methods and removed moment package

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,2 @@
 export { default as getCountdownTimer } from "./getCountdownTimer";
-export { default as getDuration } from "./getDuration";
 export { default as getRemainingTime } from "./getRemainingTime";
-export { default as showSecondsInString } from "./showSecondsInString";
